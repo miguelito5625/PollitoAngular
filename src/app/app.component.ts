001c4cb773272d459c6c9dbb05154f3c863617e0
exbinario <exbinario@OptiPlex-7040-49a4443f.localdomain>
2022-01-01 02:24:31 -0600

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,40 @@
+/**
+ * @license
+ * Copyright Akveo. All Rights Reserved.
+ * Licensed under the MIT License. See License.txt in the project root for license information.
+ */
+import { Component, OnInit } from '@angular/core';
+import { NbAuthJWTToken, NbAuthService } from '@nebular/auth';
+import { AnalyticsService } from './@core/utils/analytics.service';
+import { SeoService } from './@core/utils/seo.service';
+
+@Component({
+  selector: 'ngx-app',
+  template: '<router-outlet></router-outlet>',
+})
+export class AppComponent implements OnInit {
+
+  // constructor(private analytics: AnalyticsService, private seoService: SeoService) {
+  // }
+
+  user = {};
+
+  constructor(private authService: NbAuthService) {
+
+    this.authService.onTokenChange()
+      .subscribe((token: NbAuthJWTToken) => {
+
+        if (token.isValid()) {
+          this.user = token.getPayload(); // here we receive a payload from the token and assigns it to our `user` variable 
+          console.log(token);
+          
+        }
+
+      });
+  }
+
+  ngOnInit(): void {
+    // this.analytics.trackPageViews();
+    // this.seoService.trackCanonicalChanges();
+  }
+}
