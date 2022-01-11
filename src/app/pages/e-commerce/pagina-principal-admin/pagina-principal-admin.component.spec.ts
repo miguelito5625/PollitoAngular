1cc21fab546a9db50edc05e63c1bc94801724b8e
exbinario <miguel.4to.1994@gmail.com>
2022-01-11 16:27:45 -0600

    se comenzo a modificar la interfaz de la plantilla, se esta realizando un plantemiento de lo que llevara

---
+++ src/app/pages/e-commerce/pagina-principal-admin/pagina-principal-admin.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { PaginaPrincipalAdminComponent } from './pagina-principal-admin.component';
+
+describe('PaginaPrincipalAdminComponent', () => {
+  let component: PaginaPrincipalAdminComponent;
+  let fixture: ComponentFixture<PaginaPrincipalAdminComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ PaginaPrincipalAdminComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(PaginaPrincipalAdminComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/pages/e-commerce/pagina-principal-admin/pagina-principal-admin.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'ngx-pagina-principal-admin',
+  templateUrl: './pagina-principal-admin.component.html',
+  styleUrls: ['./pagina-principal-admin.component.scss']
+})
+export class PaginaPrincipalAdminComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
